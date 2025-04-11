0c94fd573034b80f7a538bd447f9425a6fc82694
leolai <leolai@sunsky.com.tw>
2025-04-11 15:02:06 +0800

    新增一個按鈕

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,6 +5,7 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    <q-btn color="white" text-color="black" label="Standard" />
   </q-page>
 </template>
 
