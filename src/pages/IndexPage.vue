296066a1d6e2d0baeb6018748d1920162294b198
leolai <leolai@sunsky.com.tw>
2025-04-11 15:11:25 +0800

    刪除一個按鈕

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,7 +5,6 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
-    <q-btn color="white" text-color="black" label="Standard" />
   </q-page>
 </template>
 
